d62ab0f977753454db43a3589594bf945c7d6226
Andrei <voidwarden@yahoo.com>
2019-03-11 15:02:02 +0200

    Lab 4

--- Tehnici de Programare/Recapitulare C/Laborator_4/4/main.c
+++ Tehnici de Programare/Recapitulare C/Laborator_4/4/main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int max(int *arr, int n, int m) {
-    if (n != 0) {
+int max(int a, int b) {
+    if(a < b) return a;
+    return b;
+}
 
-    } else m = arr[n];
+int maxArray(int *arr, int n, int m) {
+    if((n - 1) >= 0) {
+        if (m < arr[n - 1])
+            return maxArray(arr, n - 1, arr[n - 1]);
+        return maxArray(arr, n - 1, m);
+    } return m;
 }
 
 int main()
 {
-    printf("Hello world!\n");
+    int arr[] = {4, 5, 6, 3, 2, 8};
+    printf("%d", maxArray(arr, 6, arr[5]));
     return 0;
 }
